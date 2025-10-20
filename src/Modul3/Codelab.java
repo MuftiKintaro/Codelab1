28dab5d45cb0fc8d5b968d1b5821b23d41b4f51e
MuftiKintaro <m.kintaro2006@gmail.com>
2025-10-20 09:36:26 +0700

    thrid commit

--- src/Modul3/Codelab.java
+++ src/Modul3/Codelab.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Codelab {
     public static void main(String[] args) {
-        System.out.println("Hello World\n");
 
         Scanner input = new Scanner(System.in);
         double angka1, angka2, hasil;
